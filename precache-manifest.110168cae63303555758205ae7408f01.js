cc2deb19a2beab147189e0dc2adb6cab4193e9c6
EleniMar <elenimpea@gmail.com>
2020-07-15 20:31:12 +0300

    Updates

---
+++ precache-manifest.110168cae63303555758205ae7408f01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782618ede856464fbe0e4bcb21957b2e",
+    "url": "/movie-database-app/index.html"
+  },
+  {
+    "revision": "943d045e72f82bc2618e",
+    "url": "/movie-database-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ab1feff05840ced77b39",
+    "url": "/movie-database-app/static/css/main.6df23f46.chunk.css"
+  },
+  {
+    "revision": "943d045e72f82bc2618e",
+    "url": "/movie-database-app/static/js/2.dc9b3108.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-database-app/static/js/2.dc9b3108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1feff05840ced77b39",
+    "url": "/movie-database-app/static/js/main.a52137ae.chunk.js"
+  },
+  {
+    "revision": "753e3d2b8452fef27554",
+    "url": "/movie-database-app/static/js/runtime-main.80048382.js"
+  }
+]);
